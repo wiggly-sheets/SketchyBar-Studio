3e91b2c0af29b82341944a220789c22a3168e1ae
wiggly-sheets <zmikula@icloud.com>
2026-05-05 12:21:33 -0400

    add icon to apple bundle

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -11,8 +11,10 @@ DIST_DIR="$ROOT_DIR/dist"
 APP_BUNDLE="$DIST_DIR/$APP_NAME.app"
 APP_CONTENTS="$APP_BUNDLE/Contents"
 APP_MACOS="$APP_CONTENTS/MacOS"
+APP_RESOURCES="$APP_CONTENTS/Resources"
 APP_BINARY="$APP_MACOS/$APP_NAME"
 INFO_PLIST="$APP_CONTENTS/Info.plist"
+APP_ICON="$ROOT_DIR/Assets/SketchyBarStudio.icns"
 
 pkill -x "$APP_NAME" >/dev/null 2>&1 || true
 
@@ -20,9 +22,12 @@ swift build
 BUILD_BINARY="$(swift build --show-bin-path)/$APP_NAME"
 
 rm -rf "$APP_BUNDLE"
-mkdir -p "$APP_MACOS"
+mkdir -p "$APP_MACOS" "$APP_RESOURCES"
 cp "$BUILD_BINARY" "$APP_BINARY"
 chmod +x "$APP_BINARY"
+if [[ -f "$APP_ICON" ]]; then
+  cp "$APP_ICON" "$APP_RESOURCES/SketchyBarStudio.icns"
+fi
 
 cat >"$INFO_PLIST" <<PLIST
 <?xml version="1.0" encoding="UTF-8"?>
@@ -35,6 +40,8 @@ cat >"$INFO_PLIST" <<PLIST
   <string>$BUNDLE_ID</string>
   <key>CFBundleName</key>
   <string>SketchyBar Studio</string>
+  <key>CFBundleIconFile</key>
+  <string>SketchyBarStudio</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>LSMinimumSystemVersion</key>
